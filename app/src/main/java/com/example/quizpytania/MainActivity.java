5ee0a0a913e54623d1cacdba66b5d158e540d063
bartangutan <19bartosz11@gmail.com>
2026-09-24 10:37:15 +0200

    main activity

--- app/src/main/java/com/example/quizpytania/MainActivity.java
+++ app/src/main/java/com/example/quizpytania/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.quizpytania;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonNastepne;
+    RadioButton radioButtonA, radioButtonB, radioButtonC;
+    RadioGroup radioGroupPytania;
+    TextView textViewTresc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        buttonNastepne = findViewById(R.id.buttonNastepne);
+        radioButtonA = findViewById(R.id.radioButtonA);
+        radioButtonB = findViewById(R.id.radioButtonB);
+        radioButtonC = findViewById(R.id.radioButtonC);
+        textViewTresc = findViewById(R.id.textViewTresc);
+        radioGroupPytania = findViewById(R.id.radioGroupPytanie);
     }
 }
